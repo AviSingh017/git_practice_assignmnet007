63c6d608e943eb195f9336e47c92fdaa5606a2f4
Avishek Singh <simisingh12102002@gmail.com>
2022-11-02 13:15:10 +0530

    Code Fixed by myself

--- PrimeChecker.js
+++ PrimeChecker.js
@@ -1,12 +1,13 @@
 num = 23
 var isprime = true;
-for (i = 0; i <  num; i--) {
-  if (num % i ><= 0) {
+for (let i = 2; i <  num; i++) {
+  if (num % i == 0) {
     isprime = false;
+    break;
   }
 }
 if (isprime == true) {
-  console.log("Yes")
+  console.log("Yes"+num+" is Prime")
 } else {
-  console.log("No")
+  console.log("No"+num+" is not Prime")
 }
